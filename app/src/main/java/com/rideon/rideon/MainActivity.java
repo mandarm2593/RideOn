fd72293581a18406e4d7f504d7a64194cca8ef4f
Mandar <mmathure@syr.edu>
2017-10-20 14:28:43 -0400

    color and styles

--- app/src/main/java/com/rideon/rideon/MainActivity.java
+++ app/src/main/java/com/rideon/rideon/MainActivity.java
@@ -1,6 +1,8 @@
 package com.rideon.rideon;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.icu.util.Calendar;
 import android.net.Uri;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,6 +11,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -16,14 +19,20 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.sql.Time;
+import java.util.TimeZone;
+
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     ImageView profileimage;
-    TextView name,email;
+   TextView welcome_text,time;
+    TextView name;
     String hname,hemail;
     View header;
     DrawerLayout drawerLayout;
-    ImageButton host_button,join_button;
+    Button host_button,join_button;
     Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +40,28 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
-        host_button=(ImageButton)findViewById(R.id.host_button);
-        join_button=(ImageButton)findViewById(R.id.join_button);
+        host_button=(Button)findViewById(R.id.button_host);
+        join_button=(Button)findViewById(R.id.button_join);
+        welcome_text=(TextView)findViewById(R.id.textView_welcome);
+        name=(TextView)findViewById(R.id.textView_name);
 
+        Typeface monteserrat=Typeface.createFromAsset(getAssets(),"fonts/Montserrat-Medium.ttf");
+        welcome_text.setTypeface(monteserrat);
+       name.setTypeface(monteserrat);
+        join_button.setTypeface(monteserrat);
+        host_button.setTypeface(monteserrat);
         NavigationView navigationView;
         navigationView=(NavigationView)findViewById(R.id.navigation_view);
-
+        Calendar c=Calendar.getInstance();
+        int hour=c.get(Calendar.HOUR_OF_DAY);
+        if(hour<12)
+            welcome_text.setText("GOOD MORNING");
+        else if(hour>=12 && hour<18)
+            welcome_text.setText("GOOD AFTERNOON");
+        else
+            welcome_text.setText("GOOD EVENING");
         navigationView.setNavigationItemSelectedListener(this);
+
         drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
 
         ActionBarDrawerToggle actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.OpenDrawer,R.string.CloseDrawer);
